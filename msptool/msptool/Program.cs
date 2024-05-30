512d8a04c27e05b164c36143230d0115bca73dc3
l3c1d <159665404+l3c1d@users.noreply.github.com>
2024-05-30 08:06:09 +0100

    Add files via upload

---
+++ msptool/msptool/Program.cs
@@ -0,0 +1,83 @@
+﻿using Newtonsoft.Json.Linq;
+using System;
+using System.Collections.Generic;
+using System.IO;
+using System.Linq;
+using System.Net.Sockets;
+using System.Text;
+using System.Threading.Tasks;
+using static msptool.AMF;
+using static msptool.Checksum;
+
+namespace msptool
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.Write("Enter username: ");
+            string username = Console.ReadLine();
+
+            Console.Write("Enter password: ");
+            string password = Console.ReadLine();
+
+            Console.Write("Enter server: ");
+            string server = Console.ReadLine();
+
+            dynamic login = AMFConn(server, "MovieStarPlanet.WebService.User.AMFUserServiceWeb.Login",
+                                    new object[6] { username, password, new object[] { }, null, null, "MSP1-Standalone:XXXXXX" });
+
+            if (login["loginStatus"]["status"] != "Success")
+            {
+                Console.WriteLine("Login failed!");
+            }
+            else
+            {
+                int actorid = login["loginStatus"]["actor"]["ActorId"];
+                string ticket = login["loginStatus"]["ticket"];
+                Console.WriteLine("Login successful!");
+
+                dynamic cloth = AMFConn(server, "MovieStarPlanet.WebService.ActorClothes.AMFActorClothes.GetActorClothesRelMinimals",
+                                        new object[2]
+                                        {
+                                            new TicketHeader
+                                            {
+                                                Ticket = actor(ticket)
+                                            },
+                                            actorid
+                                        });
+
+                foreach (dynamic obj in cloth)
+                {
+                    object actorClothesRelId = obj["ActorClothesRelId"]; 
+
+                    dynamic clothinfo = AMFConn(server, "MovieStarPlanet.WebService.MovieStar.AMFMovieStarService.GetActorClothesRel",
+                                             new object[1] { actorClothesRelId });
+
+            string  shop_id = clothinfo["Cloth"]["ShopId"].ToString();
+                    string cloth_name = clothinfo["Cloth"]["Name"] ?? "Unknown";
+
+                    if (shop_id != "-100")
+                    {
+                        dynamic recycler = AMFConn(server, "MovieStarPlanet.WebService.Profile.AMFProfileService.RecycleItem",
+                                                 new object[4]
+                                                 {
+                                                     new TicketHeader
+                                                     {
+                                                         Ticket = actor(ticket)
+                                                     },
+                                                     actorid,
+                                                    actorClothesRelId,
+                                                     0
+                                                 });
+                        Console.WriteLine($"Recycled {cloth_name}");
+
+
+                    }
+                }
+                Console.WriteLine("Finished recycling. Press any key to exit.");
+                Console.ReadKey();
+            }
+        }
+    }
+}
---
+++ msptool/msptool/checksum.cs
@@ -0,0 +1,89 @@
+﻿using System;
+using System.Linq;
+using System.Security.Cryptography;
+using System.Text;
+using FluorineFx;
+using FluorineFx.AMF3;
+
+namespace msptool
+{
+    internal class Checksum
+    {
+        public static int markingID = new Random().Next(0, 1000);
+
+        public static string actor(string ticket)
+        {
+            return ticket + getMarkingId();
+        }
+
+        public static string getMarkingId()
+        {
+            markingID++;
+            byte[] bytes = Encoding.UTF8.GetBytes(markingID.ToString());
+            return BitConverter.ToString(MD5.Create().ComputeHash(bytes)).Replace("-", "").ToLower() + BitConverter.ToString(bytes).Replace("-", "").ToLower();
+        }
+        public static string createTicket(object[] parms)
+        {
+            SHA1 sha1 = SHA1.Create();
+            string tval = parms.OfType<TicketHeader>().Select(thrObj =>
+            {
+                var tps = thrObj.Ticket.Split(',');
+                return $"{tps.FirstOrDefault()}{tps.LastOrDefault()?.Substring(Math.Max(0, tps.LastOrDefault().Length - 5), Math.Min(tps.LastOrDefault().Length, 5))}";
+            }).FirstOrDefault();
+
+            if (tval == null)
+                tval = "XSV7%!5!AX2L8@vn";
+
+            string dth = $"{foi(parms)}2zKzokBI4^26#oiP{tval}";
+            byte[] hbs = sha1.ComputeHash(Encoding.UTF8.GetBytes(dth));
+            return BitConverter.ToString(hbs).Replace("-", "").ToLower();
+        }
+
+        private static string foi(object obj)
+        {
+            if (obj is TicketHeader || obj == null)
+                return "";
+
+            StringBuilder sb = new StringBuilder();
+            switch (obj)
+            {
+                case string str:
+                case int intval:
+                case long longval:
+                case double doubleval: 
+                case bool boolval:
+                    sb.Append(obj);
+                    break;
+                case DateTime dtv:
+                    sb.Append(dtv.ToString("yyyyMMdd"));
+                    break;
+                case byte[] bav:
+                    sb.Append(bav.Length <= 20 ? BitConverter.ToString(bav).Replace("-", "").ToLower() : BitConverter.ToString(bav.Take(20).ToArray()).Replace("-", "").ToLower());
+                    break;
+                case object[] arv:
+                    foreach (object item in arv)
+                        sb.Append(foi(item));
+                    break;
+                case ArrayCollection arrcv:
+                    foreach (object item in arrcv.ToArray())
+                        sb.Append(foi(item));
+                    break;
+                case ASObject as0val:
+                    sb.Append(as0val.ToString());
+                    break;
+                default:
+                    sb.Append(foi(obj));
+                    break;
+            }
+
+            return sb.ToString();
+        }
+    }
+}
+
+ internal class TicketHeader
+{
+    public string Ticket { get; set; }
+
+    public string anyAttribute { get; set; }
+}
