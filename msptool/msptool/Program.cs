cfa9b743f53654655622963393bbb8e33bdfde1e
lucid <lightsofhopee@outlook.com>
2024-05-31 20:25:41 +0100

    Added simple menu system and logout functionality

--- msptool/msptool/Program.cs
+++ msptool/msptool/Program.cs
@@ -1,11 +1,4 @@
-﻿using Newtonsoft.Json.Linq;
-using System;
-using System.Collections.Generic;
-using System.IO;
-using System.Linq;
-using System.Net.Sockets;
-using System.Text;
-using System.Threading.Tasks;
+﻿using System;
 using static msptool.AMF;
 using static msptool.Checksum;
 
@@ -15,63 +8,100 @@ namespace msptool
     {
         static void Main(string[] args)
         {
-            Console.Write("Enter username: ");
-            string username = Console.ReadLine();
+            bool loggedIn = false;
 
-            Console.Write("Enter password: ");
-            string password = Console.ReadLine();
-
-            Console.Write("Enter server: ");
-            string server = Console.ReadLine();
-
-            dynamic login = AMFConn(server, "MovieStarPlanet.WebService.User.AMFUserServiceWeb.Login",
-                                    new object[6] { username, password, new object[] { 134744072 }, null, null, "MSP1-Standalone:XXXXXX" });
-
-            if (login["loginStatus"]["status"] != "Success")
+            while (!loggedIn)
             {
-                Console.WriteLine("Login failed!");
-            }
-            else
-            {
-                int actorid = login["loginStatus"]["actor"]["ActorId"];
-                string ticket = login["loginStatus"]["ticket"];
-                Console.WriteLine("Login successful!");
+                Console.Write("Enter username: ");
+                string username = Console.ReadLine();
 
-                dynamic cloth = AMFConn(server, "MovieStarPlanet.WebService.ActorClothes.AMFActorClothes.GetActorClothesRelMinimals",
-                                        new object[2]
-                                        {
-                                            new TicketHeader { anyAttribute = null, Ticket = actor(ticket) },
-                                            actorid
-                                        });
+                Console.Write("Enter password: ");
+                string password = Console.ReadLine();
 
-                foreach (dynamic obj in cloth)
+                Console.Write("Enter server: ");
+                string server = Console.ReadLine();
+
+                dynamic login = AMFConn(server, "MovieStarPlanet.WebService.User.AMFUserServiceWeb.Login",
+                                        new object[6] { username, password, new object[] { 134744072 }, null, null, "MSP1-Standalone:XXXXXX" });
+
+                if (login["loginStatus"]["status"] != "Success")
                 {
-                    object actorClothesRelId = obj["ActorClothesRelId"]; 
+                    Console.WriteLine("Login failed [Click any key to return to login]");
+                    Console.ReadKey();
+                    Console.Clear();
+                }
+                else
+                {
+                    loggedIn = true;
+                    int actorid = login["loginStatus"]["actor"]["ActorId"];
+                    string ticket = login["loginStatus"]["ticket"];
+                    Console.WriteLine("Login successful!");
+                    Console.Clear();
 
-                    dynamic clothinfo = AMFConn(server, "MovieStarPlanet.WebService.MovieStar.AMFMovieStarService.GetActorClothesRel",
-                                             new object[1] { actorClothesRelId });
-
-            string  shop_id = clothinfo["Cloth"]["ShopId"].ToString();
-                    string cloth_name = clothinfo["Cloth"]["Name"] ?? "Unknown";
-
-                    if (shop_id != "-100")
+                    while (true)
                     {
-                        dynamic recycler = AMFConn(server, "MovieStarPlanet.WebService.Profile.AMFProfileService.RecycleItem",
-                                                 new object[4]
-                                                 { 
-                                                     new TicketHeader { anyAttribute = null, Ticket = actor(ticket) },
-                                                     actorid,
-                                                     actorClothesRelId,
-                                                     0
-                                                 });
-                        Console.WriteLine($"Recycled {cloth_name}");
+                        Console.WriteLine("\n<Home>");
+                        Console.WriteLine("1 | Recycle None-Rare Clothes");
+                        Console.WriteLine("2 | Logout");
 
+                        Console.Write("pick an option: ");
+                        string options = Console.ReadLine();
 
+                        switch (options)
+                        {
+                            case "1":
+                                recycleNoneRareClothes(server, actorid, ticket);
+                                break;
+                            case "2":
+                                Console.WriteLine("Logging out...");
+                                Console.Clear();
+                                loggedIn = false;
+                                break;
+                            default:
+                                Console.WriteLine("choose a option which exists!");
+                                break;
+                        }
+
+                        if (!loggedIn)
+                            break;
                     }
                 }
-                Console.WriteLine("Finished recycling. Press any key to exit.");
-                Console.ReadKey();
             }
+        }
+
+        static void recycleNoneRareClothes(string server, int actorid, string ticket)
+        {
+            dynamic cloth = AMFConn(server, "MovieStarPlanet.WebService.ActorClothes.AMFActorClothes.GetActorClothesRelMinimals",
+                                    new object[2]
+                                    {
+                                        new TicketHeader { anyAttribute = null, Ticket = actor(ticket) },
+                                        actorid
+                                    });
+
+            foreach (dynamic obj in cloth)
+            {
+                object actorClothesRelId = obj["ActorClothesRelId"];
+
+                dynamic clothinfo = AMFConn(server, "MovieStarPlanet.WebService.MovieStar.AMFMovieStarService.GetActorClothesRel",
+                                         new object[1] { actorClothesRelId });
+
+                string shop_id = clothinfo["Cloth"]["ShopId"].ToString();
+                string cloth_name = clothinfo["Cloth"]["Name"] ?? "Unknown";
+
+                if (shop_id != "-100")
+                {
+                    dynamic recycler = AMFConn(server, "MovieStarPlanet.WebService.Profile.AMFProfileService.RecycleItem",
+                                             new object[4]
+                                             {
+                                                 new TicketHeader { anyAttribute = null, Ticket = actor(ticket) },
+                                                 actorid,
+                                                 actorClothesRelId,
+                                                 0
+                                             });
+                    Console.WriteLine($"Recycled {cloth_name}");
+                }
+            }
+            Console.WriteLine("Finished recycling.");
         }
     }
 }
--- msptool/msptool/amf.cs
+++ msptool/msptool/amf.cs
@@ -1,4 +1,4 @@
-﻿using FluorineFx.IO;
+﻿﻿using FluorineFx.IO;
 using System;
 using System.Collections.Generic;
 using System.IO;
