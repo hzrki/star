6e7bf28eae1bbcee919a3a82dd035f6a41d1b8fe
lucid <lightsofhopee@outlook.com>
2024-06-04 12:00:20 +0100

    removed not needed

--- msptool/msptool/amf.cs
+++ msptool/msptool/amf.cs
@@ -1,11 +1,7 @@
-﻿﻿using FluorineFx.IO;
+﻿using FluorineFx.IO;
 using System;
-using System.Collections.Generic;
 using System.IO;
-using System.Linq;
 using System.Net;
-using System.Text;
-using System.Threading.Tasks;
 using static msptool.Checksum;
 
 namespace msptool
