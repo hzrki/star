7480750280b0046f5ea2682875d3bd695b91bb80
lucid <hxmzaa@outlook.com>
2024-06-26 16:00:13 +0100

    fix

--- msptoolui/msptoolui/Properties/AssemblyInfo.cs
+++ msptoolui/msptoolui/Properties/AssemblyInfo.cs
@@ -2,35 +2,18 @@
 using System.Runtime.CompilerServices;
 using System.Runtime.InteropServices;
 
-// General Information about an assembly is controlled through the following
-// set of attributes. Change these attribute values to modify the information
-// associated with an assembly.
-[assembly: AssemblyTitle("msptoolui")]
-[assembly: AssemblyDescription("")]
-[assembly: AssemblyConfiguration("")]
-[assembly: AssemblyCompany("")]
-[assembly: AssemblyProduct("msptoolui")]
-[assembly: AssemblyCopyright("Copyright ©  2024")]
-[assembly: AssemblyTrademark("")]
+[assembly: AssemblyTitle("msptoolUi")]
+[assembly: AssemblyDescription("A Multi Tool for MovieStarPlanet & MovieStarPlanet2")]
+[assembly: AssemblyConfiguration("Release")]
+[assembly: AssemblyCompany("Star NE by Lucid")]
+[assembly: AssemblyProduct("msptoolUi")]
+[assembly: AssemblyCopyright("Copyright © lucid & 6c0 2024")]
+[assembly: AssemblyTrademark("StarNE")]
 [assembly: AssemblyCulture("")]
 
-// Setting ComVisible to false makes the types in this assembly not visible
-// to COM components.  If you need to access a type in this assembly from
-// COM, set the ComVisible attribute to true on that type.
 [assembly: ComVisible(false)]
 
-// The following GUID is for the ID of the typelib if this project is exposed to COM
 [assembly: Guid("4730F4C8-CF15-40CD-9272-29299DC50E9C")]
-
-// Version information for an assembly consists of the following four values:
-//
-//      Major Version
-//      Minor Version
-//      Build Number
-//      Revision
-//
-// You can specify all the values or you can default the Build and Revision Numbers
-// by using the '*' as shown below:
-// [assembly: AssemblyVersion("1.0.*")]
-[assembly: AssemblyVersion("1.0.0.0")]
-[assembly: AssemblyFileVersion("1.0.0.0")]
+ 
+[assembly: AssemblyVersion("1.6")]
+[assembly: AssemblyFileVersion("1.6")]
