1130d8c7ea0840a2e16b7bba3f0415f5bf0d484f
lucid <hxmzaa@outlook.com>
2024-06-26 17:27:39 +0100

    a

--- msptoolui/msptoolui/home.Designer.cs
+++ msptoolui/msptoolui/home.Designer.cs
@@ -1,18 +1,17 @@
-﻿using System.ComponentModel;
+﻿using MetroFramework.Forms;
+using System;
+using System.ComponentModel;
+using System.Drawing;
+using System.IO;
+using System.Net;
+using System.Windows.Forms;
 
 namespace msptoolui
 {
     partial class home
     {
-        /// <summary>
-        /// Required designer variable.
-        /// </summary>
         private IContainer components = null;
-
-        /// <summary>
-        /// Clean up any resources being used.
-        /// </summary>
-        /// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+        private PictureBox avatarBox; 
         protected override void Dispose(bool disposing)
         {
             if (disposing && (components != null))
@@ -22,21 +21,19 @@ namespace msptoolui
 
             base.Dispose(disposing);
         }
-
-        #region Windows Form Designer generated code
-
-        /// <summary>
-        /// Required method for Designer support - do not modify
-        /// the contents of this method with the code editor.
-        /// </summary>
+        
         private void InitializeComponent()
         {
             this.components = new System.ComponentModel.Container();
             this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
             this.ClientSize = new System.Drawing.Size(800, 450);
             this.Text = "home";
-        }
 
-        #endregion
+            this.avatarBox = new PictureBox();
+            this.avatarBox.Dock = DockStyle.Left;
+            this.avatarBox.SizeMode = PictureBoxSizeMode.Zoom;
+            this.avatarBox.Size = new Size(200, 200);
+            this.Controls.Add(this.avatarBox); 
+        }
     }
 }
--- msptoolui/msptoolui/home.cs
+++ msptoolui/msptoolui/home.cs
@@ -1,4 +1,9 @@
 ﻿using MetroFramework.Forms;
+using System;
+using System.Drawing;
+using System.IO;
+using System.Net;
+using System.Windows.Forms;
 
 namespace msptoolui
 {
@@ -9,6 +14,22 @@ namespace msptoolui
         {
             InitializeComponent();
 
+
+            string avatarjpg = $"https://snapshots.mspcdns.com/v1/MSP/{server}/snapshot/fullSizeMoviestar/{actorId}.jpg";
+
+            using (WebClient msptoolui = new WebClient())
+            {
+                byte[] loc1 = msptoolui.DownloadData(avatarjpg);
+
+                using (MemoryStream loc2 = new MemoryStream(loc1))
+                {
+                    Image avatar = Image.FromStream(loc2);
+
+                    avatarBox.Image = avatar;
+                    avatarBox.SizeMode = PictureBoxSizeMode.Zoom;
+                    avatarBox.Size = new Size(200, 200); 
+                }
+            }
         }
     }
 }
