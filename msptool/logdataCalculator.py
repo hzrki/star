ba896d24dd012e5ba58c4cc4c110a1c6e7bbc9be
lcfi <159665404+lcfidev@users.noreply.github.com>
2024-08-22 22:06:57 +0100

    Update logdataCalculator.py

--- msptool/logdataCalculator.py
+++ msptool/logdataCalculator.py
@@ -1,50 +1,50 @@
 import hashlib
 
-hash_bytes = bytearray()
+hashBytes = bytearray()
 sha1 = hashlib.sha1()
-salt_characters_string = "Yd*xX#o@B15i@!th"
-salt_characters_allowed_string = "UC4#Ti#DuwkJCov)!27Po#6d-FPzIET6kAmDqMsSK3^BKzhg0p+/Zaa4Z$iI+Xl2oHåpu+XA"
-salt_characters_origin = None
-salt_characters_allowed = None
-salt_characters_allowed_copy = None
-salt_numerics_origin = None
-salt_numerics = None
-salt_numerics_bytes = bytearray()
+saltCharactersString = "Yd*xX#o@B15i@!th"
+saltCharactersAllowedString = "UC4#Ti#DuwkJCov)!27Po#6d-FPzIET6kAmDqMsSK3^BKzhg0p+/Zaa4Z$iI+Xl2oHåpu+XA"
+saltCharactersOrigin = None
+saltCharactersAllowed = None
+saltCharactersAllowedCopy = None
+saltNumericsOrigin = None
+saltNumerics = None
+saltNumericsBytes = bytearray()
 
 def getLogData():
-    global salt_characters_allowed, salt_characters_allowed_copy, salt_characters_origin, salt_numerics_origin, salt_numerics
-    if salt_numerics_origin is None:
-        salt_characters_allowed = list(salt_characters_allowed_string)
-        salt_characters_allowed_copy = list(salt_characters_allowed_string)
-        salt_characters_origin = list(salt_characters_string)
-        salt_numerics_origin = []
-        salt_numerics = [0] * 16
+    global saltCharactersAllowed, saltCharactersAllowedCopy, saltCharactersOrigin, saltNumericsOrigin, saltNumerics
+    if saltNumericsOrigin is None:
+        saltCharactersAllowed = list(saltCharactersAllowedString)
+        saltCharactersAllowedCopy = list(saltCharactersAllowedString)
+        saltCharactersOrigin = list(saltCharactersString)
+        saltNumericsOrigin = []
+        saltNumerics = [0] * 16
 
-        for _loc3_ in salt_characters_origin:
-            salt_numerics_origin.append(ord(_loc3_))
+        for loc3 in saltCharactersOrigin:
+            saltNumericsOrigin.append(ord(loc3))
 
-    for _loc2_ in range(len(salt_characters_origin)):
-        _loc4_ = salt_numerics_origin[_loc2_]
-        if _loc2_ % 2 == 0:
-            _loc4_ += _loc2_
+    for loc2 in range(len(saltCharactersOrigin)):
+        loc4 = saltNumericsOrigin[loc2]
+        if loc2 % 2 == 0:
+            loc4 += loc2
         else:
-            _loc4_ -= _loc2_
-        _loc5_ = _loc4_ % len(salt_characters_allowed_string)
-        _loc6_ = salt_characters_allowed_copy[_loc5_]
-        salt_numerics_bytes.extend(_loc6_.encode('utf-8'))
-        swap_salt_indexes(_loc5_, salt_characters_allowed_copy)
+            loc4 -= loc2
+        loc5 = loc4 % len(saltCharactersAllowedString)
+        loc6 = saltCharactersAllowedCopy[loc5]
+        saltNumericsBytes.extend(loc6.encode('utf-8'))
+        swapSaltIndexes(loc5, saltCharactersAllowedCopy)
 
-    return salt_numerics_bytes.decode('utf-8')
+    return saltNumericsBytes.decode('utf-8')
 
 
 def swapSaltIndexes(param1, param2):
-    _loc3_ = len(param2)
-    _loc4_ = (param1 + 14) % _loc3_
-    _loc5_ = _loc4_ - 1 if _loc4_ - 1 >= 0 else _loc3_ - 1
-    _loc6_ = _loc4_ + 1 if _loc4_ + 1 < _loc3_ else 0
+    loc3 = len(param2)
+    loc4 = (param1 + 14) % loc3
+    loc5 = loc4 - 1 if loc4 - 1 >= 0 else loc3 - 1
+    loc6 = loc4 + 1 if loc4 + 1 < loc3 else 0
 
-    _loc7_ = param2[_loc5_]
-    param2[_loc5_] = param2[_loc6_]
-    param2[_loc6_] = _loc7_
+    loc7 = param2[loc5]
+    param2[loc5] = param2[loc6]
+    param2[loc6] = loc7
 
 print(getLogData())
