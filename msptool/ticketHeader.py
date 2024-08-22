50c84568dc33af0639fab651c6e59c65aceb44e6
lcfi <159665404+lcfidev@users.noreply.github.com>
2024-08-22 21:39:56 +0100

    aa

--- msptool/ticketHeader.py
+++ msptool/ticketHeader.py
@@ -4,14 +4,14 @@ import binascii
 from pyamf import ASObject
 
 
-def get_marking_id() -> str:
+def getMarkingId() -> str:
     loc1 = str(random.randint(0, 999))
     loc2 = hashlib.md5(loc1.encode()).hexdigest()
     return f"{loc2}{loc1}"
 
 
-def TicketHeader(anyAttribute: str = None, ticket: str = "") -> ASObject:
-    loc1 = get_marking_id().encode()
+def ticketHeader(anyAttribute: str = None, ticket: str = "") -> ASObject:
+    loc1 = getMarkingId().encode()
     loc2 = hashlib.md5(loc1).hexdigest()
     loc3 = binascii.hexlify(loc1).decode()
     loc4 = f"{ticket}{loc2}{loc3}"
