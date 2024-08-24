c481a5172170a45d8ff3b74b7952e60bd5619013
lcfi <159665404+lcfidev@users.noreply.github.com>
2024-08-24 04:24:09 +0100

    readding legacy version

---
+++ msptool/msptool/Properties/AssemblyInfo.cs
@@ -0,0 +1,18 @@
+﻿using System.Reflection;
+using System.Runtime.InteropServices;
+
+
+[assembly: AssemblyTitle("msptool")]
+[assembly: AssemblyDescription("A Multi Tool for MovieStarPlanet & MovieStarPlanet2")]
+[assembly: AssemblyConfiguration("Release")]
+[assembly: AssemblyCompany("Star NE by lcfi")]
+[assembly: AssemblyProduct("msptool")]
+[assembly: AssemblyCopyright("Copyright © lcfi & 6c0 2024")]
+[assembly: AssemblyTrademark("StarNE")]
+[assembly: AssemblyCulture("")]
+
+[assembly: ComVisible(false)]
+[assembly: Guid("615EEC94-1C9E-417F-A14B-402C862CDD90")]
+
+[assembly: AssemblyVersion("1.7")]
+[assembly: AssemblyFileVersion("1.7")]
