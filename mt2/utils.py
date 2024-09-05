495cef4364053ba6237f4e986dabc839337f9149
lcfi <hxmzaa@outlook.com>
2024-09-06 00:54:22 +0100

    add

--- mt2/utils.py
+++ mt2/utils.py
@@ -1,5 +1,57 @@
 from enum import Enum
 from typing import Tuple, Dict
+from PyQt5 import QtWidgets, QtGui, QtCore
+from curl_cffi import requests
+import shutil
+import os
+import sys
+
+VERSION_ = "https://raw.githubusercontent.com/r-h-y/star/main/msptool/version.txt"
+RELEASE_ = "https://github.com/r-h-y/star/releases/download/v{version}/mt2.exe"
+CURRENT_VERSION = "2024.09.05.7462"
+
+
+def check_version(status_label):
+    try:
+        resp = requests.get(VERSION_)
+        LATEST_ = resp.text.strip()
+        if LATEST_ != CURRENT_VERSION:
+            status_label.setText(f"A new version ({LATEST_}) is available! Downloading the update...")
+            install_update(LATEST_, status_label)
+        else:
+            status_label.setText("You are on the latest version.")
+    except requests.RequestException as e:
+        status_label.setText(f" Contact Developer if issue continues! ")
+
+
+def install_update(version, status_label):
+    ep = RELEASE_.format(version=version)
+    try:
+        resp = requests.get(ep, stream=True)
+        resp.raise_for_status()
+
+        iu1 = "mt2_update.exe"
+        with open(iu1, "wb") as f:
+            for chunk in resp.iter_content(chunk_size=8192):
+                f.write(chunk)
+
+        status_label.setText("Update downloaded successfully!")
+        repl_exe(iu1, status_label)
+
+    except requests.RequestException as e:
+        status_label.setText(f"Contact Developer if issue continues! ")
+        sys.exit()
+
+
+def repl_exe(latest_e, status_label):
+    current_ = sys.argv[0]
+    try:
+        shutil.move(latest_e, current_)
+        status_label.setText("mt2 updated successfully! Restarting...")
+        os.execl(sys.executable, sys.executable, *sys.argv)
+    except Exception as e:
+        status_label.setText(" Contact Developer if issue continues! ")
+        sys.exit()
 
 Home = [
     (1, "Buy Boonie"),
@@ -14,6 +66,20 @@ Home = [
     (10, "Query"),
     (11, "Logout")
 ]
+
+
+def load_avatar(avatar_url, label):
+    def load_image():
+        try:
+            image_data = requests.get(avatar_url).content
+            image = QtGui.QPixmap()
+            image.loadFromData(image_data)
+
+            label.setPixmap(image.scaled(300, 400, QtCore.Qt.KeepAspectRatio))
+        except requests.RequestException as e:
+            label.setText(f" Contact Developer if issue continues! ")
+
+    QtCore.QTimer.singleShot(0, load_image)
 
 class WebServer(Enum):
     UnitedKingdom = 1
