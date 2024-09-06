8ac88bae37b2aff0ff6052e465b7e5d1329c64eb
lcfi <hxmzaa@outlook.com>
2024-09-06 01:56:13 +0100

    add

--- mt2/utils/settings.py
+++ mt2/utils/settings.py
@@ -8,11 +8,10 @@ import sys
 
 VERSION_ = "https://raw.githubusercontent.com/r-h-y/star/main/msptool/version.txt"
 RELEASE_ = "https://github.com/r-h-y/star/releases/download/v{version}/mt2.exe"
-CURRENT_VERSION = "2024.09.05.7462"
+CURRENT_VERSION = "2024.09.06.1240"
 
 
 def check_version(status_label):
-    try:
         resp = requests.get(VERSION_)
         LATEST_ = resp.text.strip()
         if LATEST_ != CURRENT_VERSION:
@@ -20,38 +19,26 @@ def check_version(status_label):
             install_update(LATEST_, status_label)
         else:
             status_label.setText("You are on the latest version.")
-    except requests.RequestException as e:
-        status_label.setText(f" Contact Developer if issue continues! ")
 
 
 def install_update(version, status_label):
     ep = RELEASE_.format(version=version)
-    try:
-        resp = requests.get(ep, stream=True)
-        resp.raise_for_status()
+    resp = requests.get(ep, stream=True)
 
-        iu1 = "mt2_update.exe"
-        with open(iu1, "wb") as f:
-            for chunk in resp.iter_content(chunk_size=8192):
+    iu1 = "mt2_update.exe"
+    with open(iu1, "wb") as f:
+        for chunk in resp.iter_content(chunk_size=8192):
                 f.write(chunk)
 
         status_label.setText("Update downloaded successfully!")
         repl_exe(iu1, status_label)
 
-    except requests.RequestException as e:
-        status_label.setText(f"Contact Developer if issue continues! ")
-        sys.exit()
-
 
 def repl_exe(latest_e, status_label):
     current_ = sys.argv[0]
-    try:
-        shutil.move(latest_e, current_)
-        status_label.setText("mt2 updated successfully! Restarting...")
-        os.execl(sys.executable, sys.executable, *sys.argv)
-    except Exception as e:
-        status_label.setText(" Contact Developer if issue continues! ")
-        sys.exit()
+    shutil.move(latest_e, current_)
+    status_label.setText("mt2 updated successfully! Restarting...")
+    os.execl(sys.executable, sys.executable, *sys.argv)
 
 Home = [
     (1, "Buy Boonie"),
